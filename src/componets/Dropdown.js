4f895154af3a07143097a5033ac41f2344f08c9a
Shifa Koti <shifakoti20@gmail.com>
2025-07-24 12:58:45 +0530

    Fixed the error

--- src/componets/Dropdown.js
+++ src/componets/Dropdown.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { productDropdown, ourstoryDropdown, userLoginDropdown, userLogoutDropdown } from "./Navitems";
 import { Link } from "react-router-dom";
@@ -75,43 +74,8 @@ export const UsLoginDropdown = () => {
         })
       }
 
-import React, { useState, useEffect } from "react";
-import {productDropdown,ourstoryDropdown,userLoginDropdown,userLogoutDropdown} from "./Navitems";
-import { Link} from "react-router-dom";
-import "./Dropdown.css";
-
-// Add this favorites counter function
-const getFavoritesCount = () => {
-    const count = Object.keys(localStorage)
-        .filter(key => key.startsWith('favorite-') && !key.includes('data'))
-        .length;
-    return count;
-};
-
-export const ProDropdown =()=>{
-    const [dropdown,setDropdown] =useState(false);
-    return <>
-    <ul className={dropdown ? "product-submenu clicked" :"product-submenu"}
-     onClick={()=> setDropdown(!dropdown)}>
-        {
-            productDropdown.map(item=>{
-                return (
-                    
-                
-              <li key={item.id} className={item.cName}
-              onClick={()=> setDropdown(false)}>
-                <Link to={item.path}>{item.title}</Link>
-                  
-              </li>
-                
-                )
-            })
-        }
-
-
     </ul>
     
-
   );
 };
 
@@ -141,86 +105,3 @@ export const UsLogoutDropdown = () => {
   </>
 
 };
-
-
-        </ul>
-            </>
-        
-          };
-
-          export const UsLoginDropdown =()=>{
-            const [dropdown,setDropdown] =useState(false);
-            const [favoritesCount, setFavoritesCount] = useState(0);
-
-            // Update favorites count when dropdown opens
-            useEffect(() => {
-                setFavoritesCount(getFavoritesCount());
-                
-                // Listen for favorites updates
-                const handleFavoritesUpdate = () => {
-                    setFavoritesCount(getFavoritesCount());
-                };
-                
-                window.addEventListener('favoritesUpdated', handleFavoritesUpdate);
-                window.addEventListener('storage', handleFavoritesUpdate);
-                
-                return () => {
-                    window.removeEventListener('favoritesUpdated', handleFavoritesUpdate);
-                    window.removeEventListener('storage', handleFavoritesUpdate);
-                };
-            }, []);
-
-            return <>
-            <ul className={dropdown ? "product-submenu clicked" :"product-submenu"}
-             onClick={()=> setDropdown(!dropdown)}>
-                {
-                    userLoginDropdown.map(item=>{
-                        return (
-                            
-                        
-                      <li key={item.id} className={item.cName}
-                      onClick={()=> setDropdown(false)}>
-                        <Link to={item.path}>
-                            {item.path === "./favorites" 
-                                ? `❤️ favorites (${favoritesCount})` 
-                                : item.title
-                            }
-                        </Link>
-                          
-                      </li>
-
-                        
-                        )
-                    })
-                }
-        
-            </ul>
-                </>
-            
-              };
-
-              export const UsLogoutDropdown =()=>{
-                const [dropdown,setDropdown] =useState(false);
-                return <>
-                <ul className={dropdown ? "product-submenu clicked" :"product-submenu"}
-                 onClick={()=> setDropdown(!dropdown)}>
-                    {
-                        userLogoutDropdown.map(item=>{
-                            return (
-                                
-                            
-                          <li key={item.id} className={item.cName}
-                          onClick={()=> setDropdown(false)}>
-                            <Link to={item.path}>{item.title}</Link>
-                              
-                          </li>
-                            
-                            )
-                        })
-                    }
-            
-                </ul>
-                    </>
-                
-                  };
-
